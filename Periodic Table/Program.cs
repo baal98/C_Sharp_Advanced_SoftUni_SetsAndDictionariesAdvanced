20bcf6ca6c9b90e96b8fc7491816665076e8d35a
Dimo Todorov <baal98@gmail.com>
2022-07-05 00:03:25 +0300

    Add files via upload

---
+++ Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                foreach (string element in input)
+                elements.Add(element);
+            }
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}

7cfd84a5d6b1a7620cc745bfc108a222f5fce1a5
Dimo Todorov <baal98@gmail.com>
2022-07-05 00:02:30 +0300

    Add files via upload

---
+++ Largest 3 Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int[] sortedIntegers = integers.OrderByDescending(n => n).Take(3).ToArray();
+
+            Console.WriteLine(String.Join(" ", sortedIntegers));
+        }
+    }
+}

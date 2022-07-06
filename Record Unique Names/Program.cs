8b6c919ce90c02cb8ef7e572a6e2bfd238427afc
Dimo Todorov <baal98@gmail.com>
2022-07-07 00:18:10 +0300

    Add files via upload

---
+++ Record Unique Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfNames = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (string name in names)
+                Console.WriteLine(name);
+
+        }
+    }
+}

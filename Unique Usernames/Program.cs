e98966298cb4d878a90d5efc63ad4fdb036fc904
Dimo Todorov <baal98@gmail.com>
2022-07-10 14:43:58 +0300

    Add files via upload

---
+++ Unique Usernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfUsers = int.Parse(Console.ReadLine());
+            HashSet<string> users = new HashSet<string>();
+
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                string user = Console.ReadLine();
+                users.Add(user);
+            }
+            foreach (string user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
